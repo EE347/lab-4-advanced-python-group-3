bc535d2748eecce6ebc22524dd75769c974c2207
Conor Farrell <farrellconor2@gmail.com>
2024-10-09 15:10:55 +0100

    Task 2

--- task2.py
+++ task2.py
@@ -10,13 +10,15 @@ class RandomNumberGenerator():
         
     def random_int(self):
         # Your code goes here
+        return random.randint(self.lower_bound, self.upper_bound)
     
     def random_float(self):
         # Your code goes here
+        return random.uniform(self.lower_bound, self.upper_bound)
     
     
 if __name__ == '__main__':
-    random_number_generator = RandomNumberGenerator(seed=10, lower_bound=0, upper_bound=100)
+    random_number_generator = RandomNumberGenerator(seed=110, lower_bound=0, upper_bound=100)
     print(random_number_generator.random_int())
     print(random_number_generator.random_float())
     
