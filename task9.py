4198518886ecb6817ef8877625355a24a11a0620
Conor Farrell <farrellconor2@gmail.com>
2024-10-09 15:56:13 +0100

    Task 9

--- task9.py
+++ task9.py
@@ -1,3 +1,31 @@
 import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
+
+date = []
+stocks = {}
+labels= []
+
+with open("task9.csv", "r") as file:
+    reader = csv.reader(file)
+    for index, row in enumerate(reader):
+
+        if index == 0:
+            labels = row
+
+            for stock in row[1:]:
+                stocks[stock] = []
+            continue
+        date.append(datetime.strptime(row[0], '%d/%m/%Y'))
+        for i,stock in enumerate(labels[1:]):
+            stocks[stock].append(float(row[i+1]))
+
+
+# print(stocks)
+
+for stock in stocks:
+    change = stocks[stock][-1]/stocks[stock][0]*100
+    change = round(stocks[stock][-1] / stocks[stock][0] * 100, 2)
+    plt.plot(date, stocks[stock], label=f"{stock}: {change}%")
+plt.legend()
+plt.show()
